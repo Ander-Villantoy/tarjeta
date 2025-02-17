349662770938f341000183f3f95133989464b1c9
Ander-Villantoy <157861181+Ander-Villantoy@users.noreply.github.com>
2025-02-17 01:04:30 -0500

    Ruta corregida

--- cartas/script.js
+++ cartas/script.js
@@ -4,8 +4,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   const textoSobre = document.querySelector(".solapaSuperior h2");
   const textoCarta = document.querySelector(".carta .texto h2");
   const buttons = document.querySelectorAll("button");
-  let loopAudio = new Audio("audio/loop.mp3"); // Ruta corregida
-  let formalAudio = new Audio("audio/formal.wav"); // Ruta corregida
+  let loopAudio = new Audio("Ander-Villantoy/tarjeta/cartas/audio/loop.mp3"); // Ruta corregida
+  let formalAudio = new Audio("Ander-Villantoy/tarjeta/cartas/audio/formal.wav"); // Ruta corregida
   loopAudio.loop = true;
 
   const params = new URLSearchParams(window.location.search);
